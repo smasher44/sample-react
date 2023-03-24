74b690a5e8ae217759e0f14b279ce89d1de08a95
smasher44 <georgelibatique819@gmail.com>
2023-03-24 09:07:24 +0800

    sample react presentation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [goal, setGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const handleInputChange = (event) => {
+    setGoal(event.target.value);
+  };
+
+  const handleAddGoal = () => {
+    setGoals((prevGoals) => [...prevGoals, goal]);
+    setGoal('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div>
+        <label htmlFor="goal">Goal</label>
+        <input type="text" id="goal" value={goal} onChange={handleInputChange} />
+        <button onClick={handleAddGoal}>Add Goal</button>
+      </div>
+      <ul>
+        {goals.map((goal, index) => (
+          <li key={index}>{goal}</li>
+        ))}
+      </ul>
     </div>
   );
 }
